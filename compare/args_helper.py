885eff7784193a4bfe94baf769d52bdc11723e7e
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2020-07-27 22:26:52 -0500

    feat(alpha): add fn to check if paths are real dirs

--- compare/args_helper.py
+++ compare/args_helper.py
@@ -1,5 +1,8 @@
 
 import argparse
+# from os import path
+import sys
+import os
 
 
 def parse_args(args):
@@ -8,3 +11,16 @@ def parse_args(args):
     parser.add_argument('dest', type=str, help='the dir where files are checked for duplicates and copied to')
     parser.add_argument('--hidden', action='store_true', help='copy hidden files also', default=False)
     return parser.parse_args(args)
+
+
+def check_args_for_real_dirs(args):
+    for arg in vars(args):
+        arg_value = getattr(args, arg)
+        if isinstance(arg_value, bool):
+            continue
+        elif os.path.isdir(arg_value):
+            continue
+        else:
+            print(f'STOP... this is not a dir, {arg}: \'{arg_value}\'')
+            sys.exit(1)
+    print('\nboth paths okay!')
