99de78c955a3491944b14793adc73e85ca74fd88
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-04-11 13:38:32 -0400

    remove Validate from main

--- compare/main.py
+++ compare/main.py
@@ -1,7 +1,6 @@
 
 import sys
 import args_helper
-from Classes import Validate
 
 
 def main():
@@ -9,16 +8,7 @@ def main():
     if sys.version_info.major < 3:
         raise ValueError("Python 3 or higher is required.")
 
-    args_obj = args_helper.parse_args(sys.argv[1:])
-
-    setup = Validate(args_obj)
-    # TODO handle "" for paths with spaces
-    if not setup.validate_input():
-        sys.exit(1)
-    else:
-        print('\nBoth paths okay!\n')
-        setup.print_paths()
-
+    args_helper.parse_args(sys.argv[1:])
 
 if __name__ == "__main__":
     main()
