bbbe5f0bdc9c63060bf0ecc33f66c64f057d642d
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-04-12 18:38:40 -0400

    add test for python3 version check

--- tests/unit_tests/test_main/test_main.py
+++ tests/unit_tests/test_main/test_main.py
@@ -23,3 +23,18 @@ def test_fail_python3_version_check(mocker):
     with pytest.raises(ValueError, match="Python 3 or higher is required."):
         # when:
         main.main()
+
+
+def test_pass_python3_version_check(mocker, fixture_args_helper):
+    """should not raise an error when main.py is called with python3"""
+    # given:
+    supported_python = FakeVersion(3)
+    # TODO review second usage of patch.object(). Should we mock the sys in the prod file?
+    mocker.patch.object(sys, "version_info", supported_python)
+
+    # when:
+    try:
+        main.main()
+    except Exception as e:
+        # then assert:
+        assert e is None
