632dd2ed3a89ce247b5a0852976c7b9d31331687
nathan leezer <62969830+GreenPlow@users.noreply.github.com>
2021-04-29 15:44:55 -0400

    add tests for issamepath method

---
+++ tests/unit_tests/test_config/test_issamepath.py
@@ -0,0 +1,66 @@
+import pytest
+from compare import config
+
+
+@pytest.fixture
+def mock_print(mocker):
+    return mocker.patch.object(config, "print")
+
+
+class FakeParseArgs:
+    """Example of an object returned from args_helper.parse_args"""
+
+    __test__ = False  # Stops pytest from warning that it cannot instantiate this class
+
+    def __init__(self, origin, destination, hidden):
+        self.origin = origin
+        self.destination = destination
+        self.hidden = hidden
+
+
+test_args_object_default = FakeParseArgs(
+    "something/directory_A", "something/directory_B", False
+)
+test_args_object_hidden_files = FakeParseArgs(
+    "something/directory_A", "something/directory_B", True
+)
+
+
+class TestClass:
+    def test_issamepath_true(self, mocker, mock_print):
+        """should return true if the paths are the same"""
+
+        # given:
+        mocker.patch.object(config, "os", autospec=True)
+        args_object_default = FakeParseArgs(
+            "something/directory_A", "something/directory_A", False
+        )
+        test_config = config.Config(args_object_default)
+
+        # when:
+        actual = test_config.issamepath()
+
+        # then assert:
+        mock_print.assert_called_once_with(
+            "\nSTOP... The origin and destination paths are the same.\n"
+        )
+        assert actual == True
+
+    def test_issamepath_false(self, mocker, mock_print):
+        """should return false if both paths are different"""
+
+        # given
+        mock_os = mocker.patch.object(config, "os", autospec=True)
+        mock_os.path.samefile.return_value = False
+
+        args_object_default = FakeParseArgs(
+            "something/directory_A", "something/directory_B", False
+        )
+        test_config = config.Config(args_object_default)
+
+        # when:
+        actual = test_config.issamepath()
+
+        # then assert:
+        mock_print.assert_not_called()
+        assert actual == False
